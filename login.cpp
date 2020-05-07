d849f6c2ba1cdcedf5a10bc464b81a52657f2f3b
yuvalkansal <yuvalkansal@gmail.com>
2020-05-07 17:26:53 +0800

    Edit to call Load function and dynamic memory management

--- login.cpp
+++ login.cpp
@@ -2,13 +2,15 @@
 #include <fstream>
 #include <string>
 #include <cstdlib>
+#include <vector>
 #include "output_content.h"
 #include "create_account.h"
 #include "login.h"
 #include "found.h"
+#include "level_loader.h"
 
 using namespace std;
-
+vector<string> temp_vector;
 string username;
 
 /* this is the login function for the user. If the account exists, 
@@ -20,7 +22,7 @@ void login()
     int err_username = 0;
     cout << "\nWelcome to the login function\n";
     cout << "Please enter your username: ";
-    getline(cin, username);
+    cin >> username;
     do
     {
         if (found(username) == 1)
@@ -36,16 +38,22 @@ void login()
         else if (found(username) == 0)
             err_username = 1;
     } while (!err_username);
-    string temp = username + ".txt";
-    fstream object(temp.c_str(), ios::in);
+    string *temp = new string;
+    (*temp) = username + ".txt";
+    fstream object((*temp).c_str(), ios::in);
     string level_check;
-    char get_ch;
+    //char get_ch;
     int get_it;
     object.seekg(0, ios::beg);
     object >> level_check;
     object >> level_check; // to get his vector form
-
-    //copy this to a vector
+    //convert to vector
+    for (int i = 0; i < level_check.length(); ++i)
+    {
+        char ch = level_check[i];
+        string ch2(1, ch);
+        temp_vector.push_back(ch2);
+    }
 
     system("clear");
     cout << "Hello " << username << endl;
@@ -55,12 +63,18 @@ void login()
     object.open(level_check.c_str(), ios::in);
 
     //display contents function
-    output_content(level_check);
+    string temp_file = level_check + ".txt";
+    output_content(temp_file);
+    object.close();
+    delete temp;
+    Load(temp_vector, username);
 }
 
+/*
 int main()
 {
     login();
 
     return 0;
 }
+*/
