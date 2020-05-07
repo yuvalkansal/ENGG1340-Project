cd29a591b29f442aed6fa0e550badc2bda393f1f
Abhigyan Kashyap <59292462+Kash1405@users.noreply.github.com>
2020-05-07 15:12:10 +0530

    Main Function

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "create_account.h"
+#include "login.h"
+#include "output_content.h"
+
+// do the help page ka system clear vaala part ---> DONE
+// store the weapon choice also ---> DONE
+// close all the files
+// make help page ---> Copy it from Abhigyan's mail
+// think of new and delete operator
+// update/beautify main page ---> DONE
+
+using namespace std;
+
+int main()
+{
+    output_content("main.txt");
+    cout << "Welcome to our game THE CASTLE. You will have an enthrilling experience\nhenceforth!!! ";
+    cout << "May the odds be ever in your favour.\nAre you an existing user or are you a new person looking for a great experience?\n";
+    cout << "Press 1 if you are an existing user and 2 if you are new: ";
+    int choice;
+    bool flag = true;
+    do
+    {
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            login();
+        case 2:
+            sign_up();
+        default:
+            cout << "Please enter a valid choice\n";
+            flag = false;
+        }
+    } while (flag == false);
+    return 0;
+}
