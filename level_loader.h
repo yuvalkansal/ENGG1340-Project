b4e3390ec53d96cdd169a690affdd264bc891e8a
Abhigyan Kashyap <59292462+Kash1405@users.noreply.github.com>
2020-05-07 15:11:39 +0530

    Header File for Level Loader

---
+++ level_loader.h
@@ -0,0 +1,11 @@
+//LEVEL_LOADER_H
+#ifndef LEVEL_LOADER_H
+#define LEVEL_LOADER_H
+
+#include <string>
+#include <vector>
+using namespace std;
+void Display(string st);
+void Load(vector<string> temp = {"0"}, string username = "");
+char getWeapon(string temp);
+#endif
